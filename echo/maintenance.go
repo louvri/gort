159fee62189cc04f3f781da7af5cfb2aea8912f9
Phillip Leonardo <mr.phillipleonardo@gmail.com>
2025-05-12 02:06:27 +0700

    add skipped path at maintenance mode (#7)
    Co-authored-by: Phillip Leonardo <phillip.leonardo@idexpress.com>

--- echo/maintenance.go
+++ echo/maintenance.go
@@ -2,12 +2,13 @@ package echo
 
 import (
 	"github.com/labstack/echo/v4"
+	"strings"
 )
 
-func ProbeMaintenanceMiddleware(errorMessage string, statusCode int, enabled bool) echo.MiddlewareFunc {
+func ProbeMaintenanceMiddleware(errorMessage, skippedPath string, statusCode int, enabled bool) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			if enabled {
+			if enabled && !strings.Contains(c.Request().URL.Path, skippedPath) {
 				return echo.NewHTTPError(statusCode, map[string]string{"message": errorMessage})
 			}
 
--- gin/maintenance.go
+++ gin/maintenance.go
@@ -2,11 +2,12 @@ package gin
 
 import (
 	"github.com/gin-gonic/gin"
+	"strings"
 )
 
-func ProbeMaintenanceMiddleware(errorMessage string, statusCode int, enabled bool) gin.HandlerFunc {
+func ProbeMaintenanceMiddleware(errorMessage, skippedPath string, statusCode int, enabled bool) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if enabled {
+		if enabled && !strings.Contains(c.Request.URL.Path, skippedPath) {
 			c.JSON(statusCode, gin.H{"message": errorMessage})
 			c.Abort()
 			return
